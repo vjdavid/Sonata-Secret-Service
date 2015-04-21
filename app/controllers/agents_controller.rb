841a4d6683eb48f22f9b862739bfdf90e9ba7866
vjdavid <vjdavid621@gmail.com>
2015-04-20 20:42:04 -0700

    finished actions of controller agents

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,2 +1,46 @@
 class AgentsController < ApplicationController
+  before_action :find_agent, only: [:show, :update, :destroy]
+  
+  def index
+    @agents = Agent.all
+    render json: @agents
+  end
+
+  def create
+    @agent = Agent.create(agent_params)
+
+    if @agent.save
+      render json: @agent, status: 200
+    else
+      render json: @agent, status: 404
+    end
+  end
+
+  def show
+    render json: @agent
+  end
+
+  def update
+    if @agent.update(agent_params)
+      render json: @agent, status: 200
+    else
+      render json: @agent, status: 404
+    end
+  end
+
+  def destroy
+    @agent.destroy
+    head :no_content
+  end
+
+  private
+
+  def agent_params
+    params.require(:agent).permit(:name, :email)
+  end
+
+
+  def find_agent
+    @agent = Agent.find(params[:id])
+  end
 end
--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,3 +1,4 @@
 class Agent < ActiveRecord::Base
+  validates :name, :email, presence: :true
   has_many :projects
 end
